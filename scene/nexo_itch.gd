ffc56bd66d999f3f0cdc24a72031c5311122e850
Timeking23 <Danielgetahun46@gmail.com>
2024-09-06 23:35:55 -0500

    rework

--- scene/nexo_itch.gd
+++ scene/nexo_itch.gd
@@ -1,53 +1,37 @@
-extends PhysicsBody3D
+extends CharacterBody3D
 
-@export var sprint_speed = 9
-@export var gravity = 5
-@export var walk_speed = 5
-@export var jump_force = 3
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
 
-#animation
+var target_velocity = Vector3.ZERO
 
-var idle_node_name = 'idle'
-var walk_node_name = 'walk'
-var run_node_name = 'run'
-var jump_node_name = 'jump'
-var death_node_name = 'death'
-var attack_node_name = 'attack'
 
-#state machine condition
-var is_attacking: bool
-var is_walking: bool
-var is_dying: bool
-var is_running: bool
+func _physics_process(delta):
+	var direction = Vector3.ZERO
 
-#physics level
-var direction : Vector3
-var horizontal_velocity: Vector3
-var aim_turn: float
-var movement: Vector3
-var vertial_velocity: Vector3
-var movement_speed: int
-var angular_acceleration:int
-var acceleration: Vector3
-var just_hit: bool
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
 
-@onready var camrot_h = get_node("camroot/h")
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.basis = Basis.looking_at(direction)
 
-func _ready() -> void:
-	pass
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		aim_turn = -event.relative.x * 0.015
-	if event.is_action_pressed("aim"):
-		direction = camrot_h.global_tranform.basis.z
-		
-func _physics_process(delta: float) -> void:
-	var on_floor = move_and_slide()
-	if !is_dying:
-		if !is_on_floor:
-			vertical_velocity += Vector3.DOWN*gravity*2*delta
-			
-	
-	
-	
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
