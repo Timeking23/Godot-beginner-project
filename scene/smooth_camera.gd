aadc8c6e35f2bd0bcc79b5181aa3f8c57bb21752
Timeking23 <Danielgetahun46@gmail.com>
2024-09-07 14:09:39 -0500

    removed redundancy

--- scene/smooth_camera.gd
+++ scene/smooth_camera.gd
@@ -11,12 +11,3 @@ func _physics_process(delta: float) -> void:
 	)
 	
 	global_position = get_parent().global_position
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
