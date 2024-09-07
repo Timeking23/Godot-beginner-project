29142927f0a842e3c65493514ff0964fa29d772e
Timeking23 <Danielgetahun46@gmail.com>
2024-09-07 00:47:38 -0500

    fIxed

---
+++ addons/fp_controller/scripts/player_move_state/fall.gd
@@ -0,0 +1,27 @@
+class_name Fall extends PlayerState
+
+## Variable for storing the state prior to falling
+var init_state: int
+
+
+func enter(msg := {}) -> void:
+	player.is_affected_by_gravity = true
+	if msg:
+		init_state = msg[state_machine.TO]
+
+
+func handle_input(_event: InputEvent) -> void:
+	if Input.is_action_just_released("sprint"):
+		init_state = state_machine.WALK
+
+
+func physics_update(_delta: float) -> void:
+	if player.is_on_floor():
+		state_machine.transition_to(state_machine.movement_state[init_state])
+	
+	if Input.is_action_pressed("jump") && player.can_climb:
+		if player.check_climbable():
+			state_machine.transition_to(
+				state_machine.movement_state[state_machine.GRAB],
+				{ "ledge_position" = player.ledge_position }
+			)
